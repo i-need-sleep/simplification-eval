6722aa0f3a26083775a271d9c8d0b85f49f4ccf2
Will <yh2689@nyu.edu>
2023-04-23 15:47:02 +0400

    initial commit

---
+++ code/utils/globals.py
@@ -0,0 +1,8 @@
+# Data
+DATA_DIR = '../data'
+PROCESSED_DIR = f'{DATA_DIR}/processed'
+
+# Results
+RESULTS_DIR = '../results'
+CHECKPOINTS_DIR = f'{RESULTS_DIR}/checkpoints'
+OUTPUTS_DIR = f'{RESULTS_DIR}/outputs'
