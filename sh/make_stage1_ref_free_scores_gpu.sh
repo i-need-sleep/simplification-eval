b1dd12359b8df1de48a96f96ebb1dc7d79d403da
yichen huang <yh2689@nyu.edu>
2023-05-31 16:35:16 +0400

    sh

--- sh/make_stage1_ref_free_scores_gpu.sh
+++ sh/make_stage1_ref_free_scores_gpu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=train_stage_2_3e-5_from_stage2_sanity       # 任务名
+#SBATCH --job-name=stage_1_ref_free_gpu       # 任务名
 #SBATCH --nodes=1                   # 这里不用动 多节点脚本请查官方文档
 #SBATCH --ntasks=1                  # 这里不用动 多任务脚本请查官方文档
 #SBATCH --cpus-per-task=4           # 要几块CPU (一般4块就够用了)
-#SBATCH --mem=256GB                 # 最大内存
-#SBATCH --time=12:00:00           # 运行时间上限
+#SBATCH --mem=64GB                 # 最大内存
+#SBATCH --time=11:59:00           # 运行时间上限
 #SBATCH --mail-type=END             # ALL / END
 #SBATCH --mail-user=yh2689@nyu.edu  # 结束之后给哪里发邮件
-#SBATCH --output=./logs/stage2/%x%A.out           # 正常输出写入的文件
-#SBATCH --error=./logs/stage2/%x%A.err            # 报错信息写入的文件
+#SBATCH --output=./logs/stage1/%x%A.out           # 正常输出写入的文件
+#SBATCH --error=./logs/stage1/%x%A.err            # 报错信息写入的文件
 #SBATCH --gres=gpu:1                # 需要几块GPU (同时最多8块)
 #SBATCH -p gpu                   # 有GPU的partition
 #SBATCH --qos=gpu-8                 # To enable the use of up to 8 GPUs
@@ -19,12 +19,5 @@ cd /l/users/yichen.huang/simplification-eval/code   # 切到程序目录
 
 echo "START"               # 输出起始信息
 source /apps/local/anaconda3/bin/activate tim          # 调用 virtual env
-python -u train_deberta.py \
-    --name stage_2_3e-5_from_stage2_sanity \
-    --stage pretrain_2 \
-    --lr 3e-5 \
-    --batch_size 10 \
-    --batch_size_dev 5 \
-    --head_type linear \
-    --checkpoint ../results/checkpoints/stage_2_3e-5_linear_from_stage1/lr3e-05_40_563_0.09335407614707947.bin
+python -u make_ref_free_supervision.py
 echo "FINISH"                       # 输出起始信息
